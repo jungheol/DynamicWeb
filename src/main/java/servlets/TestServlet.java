69303da32c1b86bd42dc2643d8bc831756285810
jungheol <jungheol@naver.com>
2024-05-21 14:20:24 +0900

    테스트 코드 추가

--- src/main/java/servlets/TestServlet.java
+++ src/main/java/servlets/TestServlet.java
@@ -1,5 +1,6 @@
 package servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +17,22 @@ public class TestServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        int num1 = 0;
+        int num2 = 0;
+        String name = "";
 
+        req.setCharacterEncoding("utf-8");
+        num1 = Integer.parseInt(req.getParameter("num1"));
+        num2 = Integer.parseInt(req.getParameter("num2"));
+        name = req.getParameter("userName");
+
+        int result = num1 + num2;
+        req.setAttribute("result", result);
+        req.setAttribute("userName", name);
+
+        //1. Dispatcher
+        RequestDispatcher re =
+                req.getRequestDispatcher("/basic/result_dispatcher.jsp");
+        re.forward(req, res);
     }
 }
